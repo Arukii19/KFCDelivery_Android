cf75f47fe2159a1ad91ccafb2753678480f00b03
Arukii19 <gian.jamye@gmail.com>
2026-05-14 01:31:59 +0800

    Add Firestore dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
 
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
